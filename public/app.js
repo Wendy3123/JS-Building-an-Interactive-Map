44f32607675dbfb23a756cc57ca9f0eec8945a30
Wendy Wang <wendywangg928@gmail.com>
2023-06-04 12:51:59 -0400

    updated

--- public/app.js
+++ public/app.js
@@ -3,30 +3,36 @@
 
 
 // Create map
-
+let map = L.map('map').setView([51.505, -0.09], 13);
 
 
 
 // Add openstreetmap tiles
 
-
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
 
 // Create and add a geolocation marker
 
-
-
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // Draw the 2nd arrondissement
-
-
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
 
 
 // Metro station markers
 
-
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+polygon.bindPopup("I am a polygon.");
 
 
 // Create red pin marker
